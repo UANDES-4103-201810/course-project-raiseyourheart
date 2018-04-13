b9b314c5e60b93ef6da71c28e8fe4660ff4b9593
cdiazarze <cdiaz1@miuandes.cl>
2018-04-13 00:05:54 -0300

    basic validations for all models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,78 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_12_191038) do
+ActiveRecord::Schema.define(version: 2018_04_13_021343) do
+
+  create_table "goods", force: :cascade do |t|
+    t.text "description"
+    t.string "name"
+    t.integer "promise_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["promise_id"], name: "index_goods_on_promise_id"
+  end
+
+  create_table "media", force: :cascade do |t|
+    t.integer "project_id"
+    t.string "type"
+    t.string "path", default: ""
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_media_on_project_id"
+  end
+
+  create_table "project_data", force: :cascade do |t|
+    t.decimal "amount_raised", default: "0.0"
+    t.boolean "visible", default: false
+    t.decimal "goal", default: "0.0"
+    t.string "category"
+    t.date "deadline"
+    t.date "estimated_delivery_time"
+    t.text "description"
+    t.integer "project_id"
+    t.string "website"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_project_data_on_project_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
+  create_table "promises", force: :cascade do |t|
+    t.text "description"
+    t.decimal "amount"
+    t.date "delivery_time"
+    t.integer "user_id"
+    t.integer "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_promises_on_project_id"
+    t.index ["user_id"], name: "index_promises_on_user_id"
+  end
+
+  create_table "user_buy_promises", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "promise_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["promise_id"], name: "index_user_buy_promises_on_promise_id"
+    t.index ["user_id"], name: "index_user_buy_promises_on_user_id"
+  end
+
+  create_table "user_fund_projects", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "project_id"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_user_fund_projects_on_project_id"
+    t.index ["user_id"], name: "index_user_fund_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "login"
@@ -26,6 +97,15 @@ ActiveRecord::Schema.define(version: 2018_04_12_191038) do
     t.string "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "wishlists", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_wishlists_on_project_id"
+    t.index ["user_id"], name: "index_wishlists_on_user_id"
   end
 
 end
