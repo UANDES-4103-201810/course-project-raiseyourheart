a67b21a8a73d60c26fc06cb5b95805927d54b6f4
Andres Howard <ahowardm@gmail.com>
2018-04-12 23:28:18 -0300

    added some relations

--- app/models/promise.rb
+++ app/models/promise.rb
@@ -1,4 +1,5 @@
 class Promise < ApplicationRecord
   belongs_to :user
   belongs_to :project
+  has_many :goods
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :projects
+  has_many :user_fund_projects
+  has_many :user_buy_promises
 end
