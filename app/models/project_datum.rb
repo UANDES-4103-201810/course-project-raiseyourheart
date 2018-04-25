c3396798b0102d616fe5e9cfe4285f7a0bf67bf5
Andres Howard <ahowardm@gmail.com>
2018-04-25 12:49:14 -0300

    seeds ready

--- app/models/project_datum.rb
+++ app/models/project_datum.rb
@@ -1,8 +1,8 @@
 class ProjectDatum < ApplicationRecord
   belongs_to :project
 
-  validates :amount_raised, numericality: {only_integer: true, greater_than_or_equal_to: 0}
-  validates :goal, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :amount_raised, numericality: { greater_than_or_equal_to: 0}
+  validates :goal, numericality: { greater_than_or_equal_to: 0}
   validates :description, length: { minimum: 50 }
   validates :deadline, :estimated_delivery_time, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,27 +19,35 @@ Project.create!([
     {user_id: 4, category_id: 4},
     {user_id: 5, category_id: 3}
 ])
-Promise.create!([
-    {description: "This is the description of the first promise", amount: "59.0", delivery_time: "2018-04-26", user_id: 1, project_id: 1},
-    {description: "This is the description of the second promise", amount: "999.0", delivery_time: "2018-04-26", user_id: 1, project_id: 1},
-    {description: "This is the description of the third promise", amount: "150.0", delivery_time: "2018-04-27", user_id: 2, project_id: 2},
-    {description: "This is the description of a promise", amount: "15.0", delivery_time: "2018-04-28", user_id: 2, project_id: 2},
-    {description: "This is the description of a promise", amount: "35.0", delivery_time: "2018-04-28", user_id: 2, project_id: 2},
-    {description: "This is the description of a promise", amount: "50.0", delivery_time: "2018-04-28", user_id: 3, project_id: 3},
-    {description: "This is the description of a promise", amount: "500.0", delivery_time: "2018-04-28", user_id: 4, project_id: 4}
-])
-Good.create!([
-  {description: "This is the description of the current good", name: "Good 1", promise_id: 1},
-  {description: "This is the description of the current good", name: "Good 2", promise_id: 1},
-  {description: "This is the description of the current good", name: "Good 2", promise_id: 2},
-  {description: "This is the description of the current good", name: "Good 4", promise_id: 3}
-])
 Medium.create!([
   {project_id: 1, multimedia_kind: "Video", path: ""},
   {project_id: 1, multimedia_kind: "Video", path: ""},
   {project_id: 3, multimedia_kind: "Photo", path: ""},
   {project_id: 4, multimedia_kind: "Photo", path: ""},
   {project_id: 4, multimedia_kind: "Photo", path: ""}
+])
+ProjectDatum.create!([
+  {amount_raised: "340.0", visible: true, goal: "3500.0", category_id: 1, deadline: "2018-04-27", estimated_delivery_time: "2018-04-28", description: "This is the description of project number 1, it has to be at least of 50 characters", project_id: 1, website: "www.project1.com"},
+  {amount_raised: "300.0", visible: true, goal: "4500.0", category_id: 2, deadline: "2018-04-28", estimated_delivery_time: "2018-04-29", description: "This is the description of project number 1, it has to be at least of 50 characters", project_id: 2, website: "www.project2.com"},
+  {amount_raised: "0.0", visible: true, goal: "500.0", category_id: 3, deadline: "2018-05-05", estimated_delivery_time: "2018-08-03", description: "This is the description of project number 3, it has to be at least of 50 characters", project_id: 3, website: "www.project3.com"},
+  {amount_raised: "10.0", visible: true, goal: "5000.0", category_id: 3, deadline: "2018-05-05", estimated_delivery_time: "2018-08-03", description: "This is the description of project number 3, it has to be at least of 50 characters", project_id: 3, website: "www.project3.com"},
+  {amount_raised: "0.0", visible: true, goal: "20000.0", category_id: 4, deadline: "2018-05-05", estimated_delivery_time: "2018-08-03", description: "This is the description of project number 4, it has to be at least of 50 characters", project_id: 4, website: "www.project4.com"},
+  {amount_raised: "0.0", visible: true, goal: "2000.0", category_id: 4, deadline: "2018-05-05", estimated_delivery_time: "2018-08-03", description: "This is the description of project number 5, it has to be at least of 50 characters", project_id: 5, website: "www.project5.com"}
+])
+Promise.create!([
+  {description: "This is the description of the first promise", amount: "59.0", delivery_time: "2018-04-26", user_id: 1, project_id: 1},
+  {description: "This is the description of the second promise", amount: "999.0", delivery_time: "2018-04-26", user_id: 1, project_id: 1},
+  {description: "This is the description of the third promise", amount: "150.0", delivery_time: "2018-04-27", user_id: 2, project_id: 2},
+  {description: "This is the description of a promise", amount: "15.0", delivery_time: "2018-04-28", user_id: 2, project_id: 2},
+  {description: "This is the description of a promise", amount: "35.0", delivery_time: "2018-04-28", user_id: 2, project_id: 2},
+  {description: "This is the description of a promise", amount: "50.0", delivery_time: "2018-04-28", user_id: 3, project_id: 3},
+  {description: "This is the description of a promise", amount: "500.0", delivery_time: "2018-04-28", user_id: 4, project_id: 4}
+])
+Good.create!([
+   {description: "This is the description of the current good", name: "Good 1", promise_id: 1},
+   {description: "This is the description of the current good", name: "Good 2", promise_id: 1},
+   {description: "This is the description of the current good", name: "Good 2", promise_id: 2},
+   {description: "This is the description of the current good", name: "Good 4", promise_id: 3}
 ])
 UserBuyPromise.create!([
   {user_id: 1, promise_id: 1},
