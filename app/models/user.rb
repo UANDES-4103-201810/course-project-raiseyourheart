2ed709dfefec769221168e61594e5c31832a4242
Andres Howard <ahowardm@gmail.com>
2018-05-04 09:55:14 -0300

    uninstall devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
   has_many :projects, dependent: :destroy
   has_many :user_fund_projects
   has_many :user_buy_promises
   has_many :wishlists, dependent: :destroy
 
-  #validates :phone, length:  {minimum: 9, maximum: 12}
+  # validates :phone, length:  {minimum: 9, maximum: 12}
   # validates :password, length: {minimum: 8, maximum: 12}
   # validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/, message: "invalid email address" }
 end
