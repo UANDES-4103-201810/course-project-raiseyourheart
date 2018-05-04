9203d45a08575eb1f4f3152c0081a9a1d3502678
Andres Howard <ahowardm@gmail.com>
2018-05-04 19:39:09 -0300

    position of footer with javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,14 @@
 //= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
+$(document).ready(function() {
+
+    var docHeight = $(window).height();
+    var footerHeight = $('.footer1').height();
+    let copyrightHeight = $('.copyright-row').height();
+    var footerTop = $('.footer1').position().top + footerHeight + copyrightHeight;
+
+    if (footerTop < docHeight) {
+        $('.footer1').css('margin-top', 10 + (docHeight - footerTop) + 'px');
+    }
+});
