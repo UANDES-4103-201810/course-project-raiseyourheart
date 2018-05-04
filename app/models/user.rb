1fc2a85c5634ed4820df9869c60c591ccabd7a96
Andres Howard <ahowardm@gmail.com>
2018-05-04 10:40:53 -0300

    devise apparently working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :projects, dependent: :destroy
   has_many :user_fund_projects
   has_many :user_buy_promises
