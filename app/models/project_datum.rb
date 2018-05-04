e4217e41b22c11b1f1af062cdc8e83b2291b652d
Andres Howard <ahowardm@gmail.com>
2018-05-04 20:57:24 -0300

    projects in landing

--- app/models/project_datum.rb
+++ app/models/project_datum.rb
@@ -6,4 +6,13 @@ class ProjectDatum < ApplicationRecord
   validates :goal, numericality: { greater_than_or_equal_to: 0}
   validates :description, length: { minimum: 50 }
   validates :deadline, :estimated_delivery_time, presence: true
+
+  def self.current_project(project)
+    project_data = where(project: project)
+    if @project.nil?
+      current_project = project_data.max_by(&:id)
+    else
+      current_project = "Ninguno"
+    end
+  end
 end
