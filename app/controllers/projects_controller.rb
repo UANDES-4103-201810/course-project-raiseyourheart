4e5157b0c936d0ad1cc5cd7665b7476b8c9fef92
cdiazarze <cdiaz1@miuandes.cl>
2018-05-05 21:09:18 -0300

    Project profile draft

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,6 +5,7 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
+    @projectdata = ProjectDatum.current_project(@project)
   end
 
   def new
