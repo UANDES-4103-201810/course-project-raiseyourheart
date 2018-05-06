734d86bc659b8d1f556c13861e25e58793332297
cdiazarze <cdiaz1@miuandes.cl>
2018-05-06 00:49:13 -0300

    late, but edit project full working

--- app/controllers/project_data_controller.rb
+++ app/controllers/project_data_controller.rb
@@ -3,23 +3,68 @@ class ProjectDataController < ApplicationController
   def update
     @project_datum=ProjectDatum.find(params[:id])
     new_params=Hash.new
-    new_params["amount_raised"]=@project_datum.amount_raised
-    new_params["visible"]=project_data_params["visible"]
-    new_params["goal"]=project_data_params["goal"]
-    new_params["category_id"]=@project_datum.category_id
-    new_params["deadline"]=project_data_params["deadline"]
-    new_params["estimated_delivery_time"]=project_data_params["estimated_delivery_time"]
-    new_params["description"]=project_data_params["description"]
-    new_params["project_id"]=@project_datum.project_id
-    new_params["website"]=project_data_params["website"]
-    new_params["name"]=project_data_params["name"]
-    new_params["avatar_file_name"]=project_data_params["avatar_file_name"]
+    if project_data_params["amount_raised"].nil?
+      new_params["amount_raised"]=@project_datum.amount_raised
+    else
+      new_params["amount_raised"]=project_data_params["amount_raised"]
+    end
+    if project_data_params["visible"].nil?
+      new_params["visible"]=@project_datum.visible
+    else
+      new_params["visible"]=project_data_params["visible"]
+    end
+    if project_data_params["goal"].nil?
+      new_params["goal"]=@project_datum.goal
+    else
+      new_params["goal"]=project_data_params["goal"]
+    end
+    if project_data_params["category_id"].nil?
+      new_params["category_id"]=@project_datum.category_id
+    else
+      new_params["category_id"]=project_data_params["category_id"]
+    end
+    if project_data_params["deadline"].nil?
+      new_params["deadline"]=@project_datum.deadline
+    else
+      new_params["deadline"]=project_data_params["deadline"]
+    end
+    if project_data_params["estimated_delivery_time"].nil?
+      new_params["estimated_delivery_time"]=@project_datum.estimated_delivery_time
+    else
+      new_params["estimated_delivery_time"]=project_data_params["estimated_delivery_time"]
+    end
+    if project_data_params["description"].nil?
+      new_params["description"]=@project_datum.description
+    else
+      new_params["description"]=project_data_params["description"]
+    end
+    if project_data_params["project_id"].nil?
+      new_params["project_id"]=@project_datum.project_id
+    else
+      new_params["project_id"]=project_data_params["project_id"]
+    end
+    if project_data_params["website"].nil?
+      new_params["website"]=@project_datum.website
+    else
+      new_params["website"]=project_data_params["website"]
+    end
+    if project_data_params["name"].nil?
+      new_params["name"]=@project_datum.name
+    else
+      new_params["name"]=project_data_params["name"]
+    end
+    if project_data_params["avatar_file_name"].nil?
+      new_params["avatar_file_name"]=@project_datum.avatar_file_name
+    else
+      new_params["avatar_file_name"]=project_data_params["avatar_file_name"]
+    end
+
     @project_datum_new=ProjectDatum.new(new_params)
 
     if @project_datum_new.save!
       redirect_to project_path(@project_datum.project_id), notice: "Project Updated!."
     else
-      redirect_to project_path(@project_datum.project_id), alert: @project_datum_new.errors.full_messages
+      redirect_to project_path(@project_datum.project_id), alert: "Project could not be Updated."
     end
     # end
   end
@@ -27,7 +72,7 @@ class ProjectDataController < ApplicationController
 
   private
   def project_data_params
-    params.permit(:amount_raised, :visible, :goal, :category_id, :deadline, :estimated_delivery_time, :description,:project_id, :website, :name, :avatar_file_name)
+    params.require(:project_datum).permit(:amount_raised, :visible, :goal, :category_id, :deadline, :estimated_delivery_time, :description,:project_id, :website, :name, :avatar_file_name)
   end
 
 end
