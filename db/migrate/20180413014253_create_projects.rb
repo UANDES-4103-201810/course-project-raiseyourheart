02d5755d89279b6f07eb396fbeef8590ed5405eb
Andres Howard <ahowardm@gmail.com>
2018-05-25 05:18:53 -0400

    featured projects

--- db/migrate/20180413014253_create_projects.rb
+++ db/migrate/20180413014253_create_projects.rb
@@ -3,6 +3,7 @@ class CreateProjects < ActiveRecord::Migration[5.2]
     create_table :projects do |t|
       t.references :user, foreign_key: true
       # t.references :category, foreign_key: true
+      t.boolean :featured, default: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2018_05_05_041503) do
 
   create_table "projects", force: :cascade do |t|
     t.integer "user_id"
+    t.boolean "featured", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
