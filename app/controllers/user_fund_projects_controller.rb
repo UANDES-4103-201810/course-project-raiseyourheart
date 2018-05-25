a2ea1514362dd5eed17e5e545cee470fb121bae0
cdiazarze <cdiaz1@miuandes.cl>
2018-05-25 00:30:29 -0400

    Review new donation

--- app/controllers/user_fund_projects_controller.rb
+++ app/controllers/user_fund_projects_controller.rb
@@ -1,9 +1,13 @@
 class UserFundProjectsController < ApplicationController
 
+  def show
+    @project = Project.find(params[:project_id])
+    @fund = @project.user_fund_projects.find(params[:id])
+  end
   def new
     @user = current_user
-    @project = Project.find(params[:id])
-    @fund = UserFundProject.New
+    @project = Project.find(params[:project])
+    @fund = @project.user_fund_projects.new
   end
 
   def create
