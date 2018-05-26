50d58bc5d3ef99731af9d79328f29bd4ebe43b29
Andres Howard <ahowardm@gmail.com>
2018-05-25 23:40:15 -0400

    create categories working

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,4 +7,19 @@ class CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to @category, notice: "Category created successfully."
+    else
+      redirect_to categories_path, alert: "Unable to create category."
+    end
+    # end
+  end
+
+  private
+      def category_params
+        params.require(:category).permit(:name, :description)
+      end
 end
