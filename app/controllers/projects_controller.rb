f1e695ba51a9531a929488c473796b443bef8758
cdiazarze <cdiaz1@miuandes.cl>
2018-05-25 23:41:22 -0400

    arreglado boton edit project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -46,11 +46,4 @@ class ProjectsController < ApplicationController
       project_params = params.require(:project).permit(:id, :category, :user, :featured)
     end
 
-    def require_owner
-      @project=Project.find(params[:id])
-      @user = current_user
-      unless (@project.user == @user || current_user_admin?)
-        redirect_to root_url, alert: "Unauthorized access!"
-      end
-    end
 end
--- app/controllers/promises_controller.rb
+++ app/controllers/promises_controller.rb
@@ -1,5 +1,5 @@
 class PromisesController < ApplicationController
-  before_action :require_owner, :except => [:show, :new, :create, :index]
+  #before_action :require_owner, :except => [:show, :new, :create, :index]
 
   def new
     @promise = Promise.new
@@ -53,12 +53,5 @@ class PromisesController < ApplicationController
     params.require(:promise).permit(:description, :delivery_time, :amount, :avatar, :project, :user)
   end
 
-  def require_owner
-    @promise=Promise.find(params[:id])
-    @user = current_user
-    unless @promise.user == @user || current_user_admin?
-      redirect_to root_url, alert: "Unauthorized access!"
-    end
-  end
 
 end
