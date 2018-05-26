846efe4b90640eb2d964225aba3203aaaa116e66
Andres Howard <ahowardm@gmail.com>
2018-05-25 23:28:21 -0400

    fix project edit page

--- app/models/project_datum.rb
+++ app/models/project_datum.rb
@@ -4,11 +4,11 @@ class ProjectDatum < ApplicationRecord
   belongs_to :project
   # belongs_to :user
 
-  #validates :amount_raised, numericality: { greater_than_or_equal_to: 0}
-  #validates :goal, numericality: { greater_than_or_equal_to: 0}
-  #validates :description, length: { minimum: 50 }
-  #validates :abstract, length: { minimum: 50, maximum: 150 }
-  #validates :deadline, :estimated_delivery_time, presence: true
+  validates :amount_raised, numericality: { greater_than_or_equal_to: 0}
+  validates :goal, numericality: { greater_than_or_equal_to: 0}
+  validates :description, length: { minimum: 50 }
+  validates :abstract, length: { minimum: 50, maximum: 150 }
+  validates :deadline, :estimated_delivery_time, presence: true
 
   def self.current_project(project)
     project_data = where(project: project)
