6765b467dabff5d013dbeb07733dc1d212306dda
Andres Howard <ahowardm@gmail.com>
2018-06-15 15:42:17 -0400

    deploying

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,5 @@ module Raiseyourheart
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    # # config/application.rb
-    config.assets.initialize_on_precompile = false
   end
 end
